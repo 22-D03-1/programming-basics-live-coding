464ff2bd0f3bc787b8db689a5c5ef6b9c8caacce
marvinscheffold <m@marvinscheffold.com>
2022-06-30 10:25:54 +0200

    variablen

--- fehler/variablen.js
+++ fehler/variablen.js
@@ -51,8 +51,8 @@ hauptstadt = "Madrid";
 /**
  *
  *
- * Ein Variablenname darf nicht mit einer Zahl (0,1,2,3,4,5,6,7,8,9)
- * beginnen
+ * Ein Variablenname darf nicht mit einer Zahl 
+ * z.B (0,1,2,3,4,5,6,7,8,9) beginnen
  *
  *
  */
