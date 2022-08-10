df758fe40ba2f3876321015c1092ebe3a5d2ef4a
marvinscheffold <m@marvinscheffold.com>
2022-08-10 10:05:42 +0200

    bekannte methoden

---
+++ objects/bekannte-methoden.js
@@ -0,0 +1,12 @@
+/**
+ * Ein Paar bekannte Methoden
+ *
+ * Diese Methoden haben wir bisher benutzt ohne zu wissen, dass es Methoden sind
+ */
+
+console.log();
+Math.random();
+Math.floor();
+// und weitere Math Methoden
+
+
